7a839e915319769896a48868e597f085619ac2ed
ParmigianaEnjoyer <nico02r@gmail.com>
2024-06-26 17:44:21 +0200

    rielaboarata la base della chat e completata, da rifinire

--- AvventuraTestuale_CheesyWizards/src/main/java/com/mycompany/avventuratestuale_cheesywizards/chat/ServerFrame.java
+++ AvventuraTestuale_CheesyWizards/src/main/java/com/mycompany/avventuratestuale_cheesywizards/chat/ServerFrame.java
@@ -4,16 +4,26 @@
  */
 package com.mycompany.avventuratestuale_cheesywizards.chat;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author nico0
  */
-public class ChatFrame extends javax.swing.JFrame {
+public class ServerFrame extends javax.swing.JFrame {
 
+    private PrintWriter out;
+    private static final int PORT = 12345; // Porta del server
+    
     /**
      * Creates new form chatFrame
      */
-    public ChatFrame() {
+    public ServerFrame() {
         initComponents();
     }
 
@@ -30,13 +40,12 @@ public class ChatFrame extends javax.swing.JFrame {
         jTextArea2 = new javax.swing.JTextArea();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        areaDestinatario = new javax.swing.JTextArea();
+        messageArea = new javax.swing.JTextArea();
         jScrollPane3 = new javax.swing.JScrollPane();
-        areaMittente = new javax.swing.JTextArea();
+        inputArea = new javax.swing.JTextArea();
         email_field = new javax.swing.JTextField();
         send_button = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        chiama_button = new javax.swing.JButton();
 
         jTextArea2.setColumns(20);
         jTextArea2.setRows(5);
@@ -46,33 +55,30 @@ public class ChatFrame extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(51, 51, 51));
 
-        areaDestinatario.setBackground(new java.awt.Color(102, 102, 102));
-        areaDestinatario.setColumns(20);
-        areaDestinatario.setForeground(new java.awt.Color(0, 0, 0));
-        areaDestinatario.setRows(5);
-        jScrollPane1.setViewportView(areaDestinatario);
+        messageArea.setBackground(new java.awt.Color(102, 102, 102));
+        messageArea.setColumns(20);
+        messageArea.setForeground(new java.awt.Color(0, 0, 0));
+        messageArea.setRows(5);
+        jScrollPane1.setViewportView(messageArea);
 
-        areaMittente.setBackground(new java.awt.Color(0, 0, 0));
-        areaMittente.setColumns(20);
-        areaMittente.setForeground(new java.awt.Color(255, 255, 255));
-        areaMittente.setRows(5);
-        jScrollPane3.setViewportView(areaMittente);
+        inputArea.setBackground(new java.awt.Color(0, 0, 0));
+        inputArea.setColumns(20);
+        inputArea.setForeground(new java.awt.Color(255, 255, 255));
+        inputArea.setRows(5);
+        jScrollPane3.setViewportView(inputArea);
 
         send_button.setText("INVIA");
-
-        jButton1.setBackground(new java.awt.Color(51, 153, 255));
-        jButton1.setText("RISPONDI");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        send_button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                send_buttonActionPerformed(evt);
             }
         });
 
-        jButton2.setBackground(new java.awt.Color(51, 204, 0));
-        jButton2.setText("CHIAMA");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        chiama_button.setBackground(new java.awt.Color(0, 102, 0));
+        chiama_button.setText("CHIAMA");
+        chiama_button.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                chiama_buttonActionPerformed(evt);
             }
         });
 
@@ -89,11 +95,10 @@ public class ChatFrame extends javax.swing.JFrame {
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(send_button))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jButton2)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(email_field, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(chiama_button)
+                        .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -102,15 +107,14 @@ public class ChatFrame extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(email_field, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(chiama_button))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 16, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(send_button)
-                .addContainerGap())
+                .addContainerGap(10, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -127,61 +131,86 @@ public class ChatFrame extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void chiama_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_chiama_buttonActionPerformed
+        //rispondi_button.setEnabled(false);
+        if (email_field.getText() != null && !email_field.getText().isBlank() && !email_field.getText().isEmpty()){
+            String user_I_want = email_field.getText();
+            
+            System.out.println("Server avviato...");
+            new Thread(() -> {
+                try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+                    System.out.println("In attesa di connessioni sulla porta " + PORT + "...");
+                    while (true) {
+                        Socket clientSocket = serverSocket.accept();
+                        System.out.println("Connessione in ingresso...");
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                        out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+                        String clientName = in.readLine();
+                        System.out.println("Nome client ricevuto: " + clientName);
+
+                        if (user_I_want.equals(clientName)) {
+                            System.out.println("Client autorizzato!");
+                            messageArea.append("Client autorizzato!\n");
+
+                            Thread readerThread = new Thread(() -> {
+                                try {
+                                    String inputLine;
+                                    while ((inputLine = in.readLine()) != null) {
+                                        messageArea.append("Client: " + inputLine + "\n");
+                                    }
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            });
+                            readerThread.start();
+                        } else {
+                            System.out.println("Client non autorizzato. Connessione chiusa.");
+                            messageArea.append("Client non autorizzato. Connessione chiusa.\n");
+                            out.println("Nome utente non autorizzato.");
+                            clientSocket.close();
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }//GEN-LAST:event_chiama_buttonActionPerformed
+
+    private void send_buttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_send_buttonActionPerformed
+        if (out != null) {
+            out.println(inputArea.getText());
+            messageArea.append("Server: " + inputArea.getText() + "\n");
+            inputArea.setText("");
+        }
+    }//GEN-LAST:event_send_buttonActionPerformed
 
     /**
-     * @param args the command line arguments
+     * 
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ChatFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ChatFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ChatFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ChatFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-        //</editor-fold>
+    public void runServerFrame() {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new ChatFrame().setVisible(true);
+                new ServerFrame().setVisible(true);
             }
         });
+        
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JTextArea areaDestinatario;
-    private javax.swing.JTextArea areaMittente;
+    private javax.swing.JButton chiama_button;
     private javax.swing.JTextField email_field;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JTextArea inputArea;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextArea messageArea;
     private javax.swing.JButton send_button;
     // End of variables declaration//GEN-END:variables
 }
